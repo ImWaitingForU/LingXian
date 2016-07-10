6584cb94df888d1cf9da49a9510cae33950802fc
chan <870536460@qq.com>
2016-07-10 20:08:59 +0800

    实现底部导航+底部按钮

---
+++ app/src/main/java/com/lingxian/lingxian/fragment/WodeFragment.java
@@ -0,0 +1,29 @@
+package com.lingxian.lingxian.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lingxian.lingxian.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WodeFragment extends Fragment {
+
+
+    public WodeFragment () {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate (R.layout.fragment_wode, container, false);
+    }
+
+}
