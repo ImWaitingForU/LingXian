a915df80e88e9a6da48f3a4d9bf6f4fca32215e7
wxj1018 <605074293@qq.com>
2016-07-12 20:10:06 +0800

    添加登陆，注册，点子，我的界面，改进细节

--- app/src/main/java/com/lingxian/lingxian/fragment/WodeFragment.java
+++ app/src/main/java/com/lingxian/lingxian/fragment/WodeFragment.java
@@ -2,6 +2,7 @@ package com.lingxian.lingxian.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,18 +13,33 @@ import com.lingxian.lingxian.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WodeFragment extends Fragment{
+public class WodeFragment extends Fragment {
 
+    //private Button quit;
+    //private Context context;
 
-    public WodeFragment () {
-        // Required empty public constructor
+    public WodeFragment() {
+
     }
 
+    //TODO:内部按钮和控件还没实现功能
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_wode, container, false);
+    }
 
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate (R.layout.fragment_wode, container, false);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //quit = (Button) view.findViewById(R.id.id_btn_quit);
+        initEvents();
+
     }
 
+    //TODO:控件功能待定
+    private void initEvents() {
+    }
 }
